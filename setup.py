fc7069c62b336bd4f5a8883e7c2e887c682d0203
Michael Richardson <michael@mtrichardson.com>
2010-10-13 17:28:04 -0700

    Bump versions for release as well as update install_requires to add python-dateutil.

--- setup.py
+++ setup.py
@@ -4,12 +4,12 @@ from setuptools import setup
 
 
 setup(name="pubsubsuperfeedr",
-      version="0.2.0",
+      version="0.3.0",
       description="Library for working with Superfeedr's PubSubHubbub API",
       author="Urban Airship",
       author_email="contact@urbanairship.com",
       url="http://github.com/urbanairship/pubsubsuperfeedr/",
       py_modules=["pubsubsuperfeedr"],
-      install_requires=["feedparser"],
+      install_requires=["feedparser", "python-dateutil"],
       license="MIT License",
 )
